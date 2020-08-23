9559dd9dc8030d46399a7e07ce058de0ec1d8427
Hibiki Mizuno <hibiki990523@gmail.com>
2020-08-23 23:15:55 +0900

    fix

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -17,7 +17,7 @@
                     cols="12"
                     tag="h1"
                   >
-                    <p>
+                    <p class="haikei">
                     <span
                       style="font-weight:bold;"
                       class=""
@@ -62,5 +62,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.haikei{
+  background: rgba(104, 232, 255, 0.3);
 }
 </style>
