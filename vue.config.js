609b01ab570294ce6fcae227b8247bf0d8887b08
Hibiki Mizuno <hibiki990523@gmail.com>
2020-08-24 01:11:03 +0900

    sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,25 @@
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+const paths = [
+    {
+        path: '/',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: '0.8',
+        changefreq: 'hourly'
+    },
+];
 module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
   configureWebpack: {
+    plugins: [
+      new SitemapPlugin('https://mywebsite.com', paths, {
+          filename: 'sitemap.xml',
+          lastmod: true,
+          changefreq: 'hourly',
+          priority: '0.8'
+      })
+  ]
   },
   publicPath: './'
 }
